ff3fec12aa1a62a8ee1e9926e15a0618bd151b64
DEADLY-FIGHTERS <87220607+DEADLY-FIGHTERS@users.noreply.github.com>
2021-09-08 09:55:33 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -53,14 +53,14 @@ for name in files:
 
 import userbot._core
 
-print(f"""DeadlyGhouls IS ON!!! DeadlyGhouls VERSION :- {deadlyversion} YOUR ∂єα∂ℓу gнσυℓѕ IS READY TO USE! FOR CHECK YOUR BOT WORKING OR NOT PLEASE TYPE (.alive/.ping) ENJOY YOUR BOT! JOIN FOR MORE FUTURE UPDATES @DEADLY_TECHY .""")
+print(f"""CONGRATULATIONS 🥳🥳🎊🎊 YOUR DEADLY GHOULS BOT IS DEPLOYED 🎊 ... NOW TYPE .ping OR .alive TO CHECK OUR AMAZING BOT 🥳🔥 IF U HAVE ANY PROBLEM THEN JOIN @DEADLY_USERBOT""")
 async def deadly_is_on():
     try:
         if Config.PM_LOGGR_BOT_API_ID != 0:
             await bot.send_file(
                 Config.PM_LOGGR_BOT_API_ID,
                 DEADLY_GHOULS_PIC,
-                caption=f"༆ʟɛɢɛռɖaʀʏ ᴀғ ∂єα∂ℓу gнσυℓѕ༆\n\n**𝚅𝙴𝚁𝚂𝙸𝙾𝙽 ➪ {deadlyversion}**\n\n𝐓𝐲𝐩𝐞 `.ping` or `.alive` 𝐭𝐨 𝐜𝐡𝐞𝐜𝐤! \n\n𝙹𝙾𝙸𝙽 [DeadlyGhouls 𝙲𝙷𝙰𝚃](t.me/DeadlyGhouls_CHIT_CHAT) 𝚃𝙾 𝚀𝚄𝙴𝚁𝚈 & 𝙹𝙾𝙸𝙽 [𝙼𝙰𝙵𝙸𝙰 𝚄𝙿𝙳𝙰𝚃𝙴𝚂](t.me/DEADLY_TECHY) 𝚃𝙾 𝙺𝙽𝙾𝚆 𝚁𝙴𝙶𝚁𝙰𝙳𝙸𝙽𝙶 𝚄𝙿𝙳𝙰𝚃𝙴 𝙰𝙽𝙳 𝙽𝙴𝚆𝚂 𝙰𝙱𝙾𝚄𝚃 DeadlyGhouls",
+                caption=f"᯽ ᒪᗴᘜᗴᑎᗪᖇY ᗩᖴ ᗪᗴᗩᗪᒪY ᘜᕼOᑌᒪՏ ᯽\n\n**𝚅𝙴𝚁𝚂𝙸𝙾𝙽 ➪ {deadlyversion}**\n\n𝚃𝚈𝙿𝙴 `.ping` or `.alive` 𝚃𝙾 𝙲𝙷𝙴𝙲𝙺! \n\n𝙹𝙾𝙸𝙽 [𝙳𝙴𝙰𝙳𝙻𝚈 𝙶𝙷𝙾𝚄𝙻𝚂 𝙲𝙷𝙰𝚃](t.me/DeadlyGhouls_CHIT_CHAT) 𝚃𝙾 𝚀𝚄𝙴𝚁𝚈 & 𝙹𝙾𝙸𝙽 [𝙳𝙴𝙰𝙳𝙻𝚈 𝚄𝙿𝙳𝙰𝚃𝙴𝚂](t.me/DEADLY_TECHY) 𝚃𝙾 𝙺𝙽𝙾𝚆 𝚁𝙴𝙶𝚁𝙰𝙳𝙸𝙽𝙶 𝚄𝙿𝙳𝙰𝚃𝙴 𝙰𝙽𝙳 𝙽𝙴𝚆𝚂 𝙰𝙱𝙾𝚄𝚃 𝙳𝙴𝙰𝙳𝙻𝚈 𝙶𝙷𝙾𝚄𝙻𝚂 𝙱𝙾𝚃",
             )
     except Exception as e:
         LOGS.info(str(e))
@@ -68,5 +68,5 @@ async def deadly_is_on():
 bot.loop.create_task(deadly_is_on())
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
-else:
+else:            
     bot.run_until_disconnected()
