cb56a722653a209dca4949e9e6c6aeb1a80b907d
DEADLY-FIGHTERS <DEADLY-FIGHTERS@users.noreply.github.com>
2021-09-08 04:02:36 +0000

    MA KI CHUT VRMO🙈💋

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -10,7 +10,7 @@ from userbot.utils import admin_cmd, sudo_cmd
 from telethon.tl.types import ChannelParticipantsAdmins
 from userbot.cmdhelp import CmdHelp
 # 🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "𝕄𝔸𝔽𝕀𝔸𝔹𝕆𝕋"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "DᴇᴀᴅʟʏGʜᴏᴜʟs"
 
 ludosudo = Config.SUDO_USERS
 
